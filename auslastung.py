28940f93e9bc67af9965fcb90fee057b3fe1affe
therealgherkhin <lscholz14@googlemail.com>
2020-10-13 17:57:40 +0200

    created logging

--- auslastung.py
+++ auslastung.py
@@ -61,15 +61,28 @@ import csv
 with open('data.csv', mode='a') as file:
     fieldnames = ['date', 'time', 'studio', 'auslastung']
     writer = csv.DictWriter(file, fieldnames=fieldnames)
-    today_warning = date.today()
+    date_warning = date.today()
+    time_warning = datetime.now()
 
     for studio in studios:
         try:
             print(studio)
             writer.writerow(get_auslastung(studio))
+            with open('log.txt', mode='a') as log:
+                logfile_succ = "SUCCESFULL!:" + "," + str(date_warning) + "," + str(time_warning) + "," + studio + "\n"
+                log.write(logfile_succ)
+                log.close()
         except Exception:
-            print("!!!!RETRYING!!!!" + today_warning)
+            print("!!!!RETRYING!!!!")
+            with open('log.txt', mode='a') as log:
+                logfile_err= "ERROR!: " + "," + str(date_warning) + "," + str(time_warning) + "," + studio + "\n"
+                log.write(logfile_err)
+                log.close()
             writer.writerow(get_auslastung(studio))
+
             #print("Exception: Continue")
             #continue
+
+
+
 
